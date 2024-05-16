b13e8e51a9c2571b80d797863d9ff4ffc6e66ec1
HarithaAkil <harithabscit@gmail.com>
2024-05-16 13:52:57 +0530

    update project

---
+++ src/main/java/org/classbase/Datadriv6.java
@@ -0,0 +1,14 @@
+package org.classbase;
+import java.io.IOException;
+
+public class Datadriv6 extends Baseclass{
+	public static void main(String[] args) throws IOException {
+		createNewExcelFile(0,0,"Testing");
+		createRow(0,1,"datadrive");
+		createRow(0,2,"cucumber");
+		createRow(0,3,"java");
+		
+	}
+
+}
+
